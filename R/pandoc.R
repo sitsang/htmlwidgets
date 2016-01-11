3810f588c88e9d754b7642e4a18c31c22768e3cb
sitsang <simon.tsang@scotiabank.com>
2016-01-11 16:40:55 -0500

    Update pandoc.R

--- R/pandoc.R
+++ R/pandoc.R
@@ -79,7 +79,7 @@ pandoc_convert <- function(input,
   args <- c(args, options)
 
   # set pandoc stack size
-  stack_size <- getOption("pandoc.stack.size", default = "5G")
+  stack_size <- getOption("pandoc.stack.size", default = "4294967295")
   args <- c(c("+RTS", paste0("-K", stack_size), "-RTS"), args)
 
   # build the conversion command
